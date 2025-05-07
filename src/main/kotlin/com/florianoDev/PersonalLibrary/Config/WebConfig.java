3462d0288d5054cb06c18095e5963ccba27ffe7e
Erne1984 <ernestofamorim@gmail.com>
2025-05-07 20:54:14 -0300

    feat: Web config

--- src/main/kotlin/com/florianoDev/PersonalLibrary/Config/WebConfig.java
+++ src/main/kotlin/com/florianoDev/PersonalLibrary/Config/WebConfig.java
@@ -9,10 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permite todas as URLs
-                .allowedOrigins("http://localhost:5173") // Permite apenas requisições de localhost:5173 (onde seu frontend está rodando)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-                .allowedHeaders("*") // Permite todos os headers
-                .allowCredentials(true); // Permite credenciais, como cookies
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
